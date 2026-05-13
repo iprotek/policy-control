0791a4c63322311515b7f441c84bf995eac512f5
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-05-13 23:39:21 +0800

    Update PolicyControlController.php

--- src/Http/Controllers/PolicyControlController.php
+++ src/Http/Controllers/PolicyControlController.php
@@ -3,8 +3,17 @@
 namespace iProtek\PolicyControl\Http\Controllers;
 
 use Illuminate\Http\Request;
+use iProtek\Core\Http\Controllers\_Common\_CommonController;
+use iProtek\PolicyControl\Models\PolicyControl;
 
-class PolicyControlController extends Controller
+class PolicyControlController extends _CommonController
 {
     //
+    public function list(Request $request)
+    {
+        $data = PolicyControl::whereRaw('name like ? AND is_visible = 1',["api.%"])->orderBy('name')->get();
+
+        return $data;
+    }
+    
 }
